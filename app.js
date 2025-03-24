9975b2107d56fa7dcf31019970885caab2fb4fb9
rclarke87 <rutger.clarke@gmail.com>
2025-03-24 14:47:21 +0000

    theme finalised

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const card = document.querySelector(".col.wobble .card"); // Selects the limited-time offer card
-
-  function triggerWobble() {
-    card.classList.add("wobble");
-    setTimeout(() => card.classList.remove("wobble"), 600); // Remove class after animation
-  }
-
-  // Wobble every 5 seconds
-  setInterval(triggerWobble, 5000);
-
-  // Trigger when scrolling near it
-  document.addEventListener("scroll", function () {
-    const rect = card.getBoundingClientRect();
-    if (rect.top < window.innerHeight && rect.bottom > 0) {
-      triggerWobble();
-    }
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
   const hero = document.getElementById("hero");
   const highResImage = new Image();
   highResImage.src = "images/hero.webp";
--- profile.js
+++ profile.js
@@ -10,11 +10,13 @@ const userProfile = {
   },
   accountDetails: {
     username: "rutgerclake",
+    accountType: "Developer",
     accountCreated: "2022-03-10T14:30:45Z",
     lastLogin: "2025-03-23T09:15:22Z",
     isVerified: true,
     subscriptionStatus: "active",
-    subscriptionTier: "lifetime",
+    subscriptionTier: "Premium",
+    lifetimeMember: true,
     admin: true,
   },
   preferences: {
@@ -26,6 +28,8 @@ const userProfile = {
     },
   },
   stats: {
+    userLevel: 12,
+    xp: 6500,
     postsCount: 0,
     followersCount: 0,
     followingCount: 0,
